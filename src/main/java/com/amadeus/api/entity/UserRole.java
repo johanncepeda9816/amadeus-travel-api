a2bc2e31a7a17d96404c3a9d028a1c1a5d96a87e
johanncepeda <profiledx16@hotmail.com>
2025-08-26 12:11:08 -0500

    feat: creating entities, enums and repository

---
+++ src/main/java/com/amadeus/api/entity/UserRole.java
@@ -0,0 +1,16 @@
+package com.amadeus.api.entity;
+
+public enum UserRole {
+    ADMIN("admin"),
+    USER("user");
+
+    private final String value;
+
+    UserRole(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
